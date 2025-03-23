23d0a5b4dae9e336a1cb13968ad22226ef62210d
Felipe <felpsmarceli@gmail.com>
2025-03-23 20:12:24 -0300

    Atribuindo as imagens em 'public', e criando divs  no qual elas foram atribuidas, cada uma com 1 titulo

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -4,7 +4,29 @@ const Produtos =() => {
 
     return(
     <section>
-        <img  width={300} src="/img1.jpg" alt="" />
+
+        <h1>Jamal</h1>
+      <div style={{ display: 'flex', gap: '16px' }}>
+        <div>
+          <h3>Jamal</h3>
+          <img height={300} width={300} src="./img1.jpg" alt="Jamal" />
+        </div>
+
+        <div>
+          <h3>Lebron Shirt</h3>
+          <img height={300} width={300} src="./lebron.png" alt="Lebron" />
+        </div>
+
+        <div>
+          <h3>Poatan Shirt</h3>
+          <img height={300} width={300} src="./poatan.png" alt="Poatan" />
+        </div>
+
+        <div>
+          <h3>Detran Shirt</h3>
+          <img height={300} width={300} src="./detran.png" alt="Detran" />
+        </div>
+      </div>
         
     </section>
     )
