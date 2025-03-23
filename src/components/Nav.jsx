e0539c1ae095a6b2c9a796a3ced5e6002a922e03
Felipe <felpsmarceli@gmail.com>
2025-03-23 19:39:52 -0300

    Estilizando com flex e colocando uma gapde 16px

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,12 @@ const Nav =() => {
     return (
   
     <section className="nav">
+      <div style={{ display: "flex", gap: '16px'}}>
       <Link to="/home" className="tlink">Home</Link>
        <Link to="/produto" className="tlink">Produto</Link>
        <Link to="/sobre" className="tlink">Sobre</Link>
        <Link to="/footer" className="tlink"></Link>
+       </div>
     </section>
   
     )
