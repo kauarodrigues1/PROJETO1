c0fe4737e3d50cfceae3d988678155b49b53e943
Felipe <felpsmarceli@gmail.com>
2025-03-23 20:23:31 -0300

    Editando a pagina sobre com imagem e paragrafo

---
+++ src/components/Sobre.jsx
@@ -0,0 +1,18 @@
+const Sobre = () => {
+    return (
+      <section>
+        <div style={{marginTop: '20px'}}>
+            <img width={800} src="./loja.png" alt="Loja"/>
+        
+            <h2>
+            Bem-vindo à nossa Loja de Esportes! Somos a loja especializada em oferecer o melhor para todos os amantes do esporte. Aqui, você encontra uma ampla gama de produtos para praticar qualquer atividade esportiva, desde os mais populares até os menos conhecidos. Seja você um corredor, um jogador de futebol, um praticante de ioga, ou um ciclista, temos tudo o que você precisa para melhorar seu desempenho e aproveitar ao máximo o que faz de melhor. Nossa missão é fornecer equipamentos de alta qualidade, roupas e acessórios, sempre com as últimas inovações do mercado esportivo. Estamos constantemente buscando parcerias com as melhores marcas para garantir que nossos clientes tenham acesso às últimas tendências e tecnologias. Cada produto que oferecemos foi escolhido a dedo para atender às necessidades e exigências dos atletas, amadores ou profissionais, com foco na durabilidade e desempenho.
+            Bem-vindo à nossa Loja de Esportes! Somos a loja especializada em oferecer o melhor para todos os amantes do esporte. Aqui, você encontra uma ampla gama de produtos para praticar qualquer atividade esportiva, desde os mais populares até os menos conhecidos. Seja você um corredor, um jogador de futebol, um praticante de ioga, ou um ciclista, temos tudo o que você precisa para melhorar seu desempenho e aproveitar ao máximo o que faz de melhor. Nossa missão é fornecer equipamentos de alta qualidade, roupas e acessórios, sempre com as últimas inovações do mercado esportivo. Estamos constantemente buscando parcerias com as melhores marcas para garantir que nossos clientes tenham acesso às últimas tendências e tecnologias. Cada produto que oferecemos foi escolhido a dedo para atender às necessidades e exigências dos atletas, amadores ou profissionais, com foco na durabilidade e desempenho.
+            Bem-vindo à nossa Loja de Esportes! Somos a loja especializada em oferecer o melhor para todos os amantes do esporte. Aqui, você encontra uma ampla gama de produtos para praticar qualquer atividade esportiva, desde os mais populares até os menos conhecidos. Seja você um corredor, um jogador de futebol, um praticante de ioga, ou um ciclista, temos tudo o que você precisa para melhorar seu desempenho e aproveitar ao máximo o que faz de melhor. Nossa missão é fornecer equipamentos de alta qualidade, roupas e acessórios, sempre com as últimas inovações do mercado esportivo. Estamos constantemente buscando parcerias com as melhores marcas para garantir que nossos clientes tenham acesso às últimas tendências e tecnologias. Cada produto que oferecemos foi escolhido a dedo para atender às necessidades e exigências dos atletas, amadores ou profissionais, com foco na durabilidade e desempenho.
+            </h2>
+        </div>
+      </section>
+    )
+  }
+  
+  export default Sobre
+  
